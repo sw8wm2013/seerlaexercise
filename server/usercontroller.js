cda1f8e6451c27d5c8ef9a5edbc8aeed718936ca
Jacob <jacobjeffreybanks@gmail.com>
2019-09-17 22:49:12 -0700

    finished the user route, but need some more information

---
+++ server/usercontroller.js
@@ -0,0 +1,19 @@
+const userDB = require('./models/usermodel'); 
+
+const userController = {
+getProfileDetails: (req, res, next) => {
+    userDB.query('SELECT * FROM users;', (err, data) => {
+      if (err) {
+        return next(err);
+      } else {
+        const users = data.rows;
+        res.locals.allUsers = users;
+        return next();
+      }
+    })
+  },
+}
+
+
+
+module.exports = userController;
