59326381d24aad8b6ca4f7953da82271937b08a9
Eric Stallings <stallings.eric@gmail.com>
2019-09-18 17:11:21 -0700

    Committing after React Router explorations.

--- client/App.jsx
+++ client/App.jsx
@@ -3,14 +3,18 @@ import SignIn from './signIn.jsx';
 import SignUp from './signUp.jsx';
 import Explore from './explore.jsx';
 import Profile from './profile.jsx';
+import {Route, Switch} from 'react-router-dom';
+
 
 const App = (props) => {
   return(
     <div>
-      <SignIn/>
-      <SignUp/>
-      <Explore/>
-      <Profile/>
+      {/* <Switch> */}
+        <Route exact path = '/' component = {SignIn}/>
+        <Route path = '/signUp' component = {SignUp}/>
+        <Route path = '/profile' component = {Profile}/>
+        <Route path = '/explore' component = {Explore}/>
+      {/* </Switch> */}
     </div>
   );
 }
